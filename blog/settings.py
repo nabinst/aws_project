0a35ecf540ef9dbb47c8f5c991c17febf63b4285
Shrestha <AWH14@CORP.UPC>
2019-11-15 14:05:48 -0500

    Added EB database config and requrements files

--- blog/settings.py
+++ blog/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -124,7 +135,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static_in_env'),]
 VEN_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VEN_PATH, 'static_root')
+#STATIC_ROOT = os.path.join(VEN_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 MEDIA_ROOT = os.path.join(VEN_PATH, 'media_root')
 
 TINYMCE_DEFAULT_CONFIG = {
